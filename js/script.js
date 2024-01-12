da4cecaedd1f124c2aa6a7c8d561973f481b13ee
Alina Bychkova <alinka030398@mail.ru>
2024-01-12 15:05:55 +0300

    Add new functions

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
 function overlay() {
     document.body.classList.toggle('show-before');
 }
- 
-function lower1() {
-    document.getElementById('lower').classList.toggle('lower1');
-}
-
-function lower2() {
-    document.getElementById('lower').classList.toggle('lower2');
-}
